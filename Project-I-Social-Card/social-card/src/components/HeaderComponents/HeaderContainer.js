0b5cc1f836ad5c5331766d59494ae749603c000f
Vijay Das <vijaytdas@gmail.com>
2019-04-22 17:56:32 -0500

    add ImageThumbnail

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail'
+
+
+//<HeaderTitle />
+//<HeaderContent />
+//<ImageThumbnail />
+
+
+function HeaderContainer() {
+    return (
+        <div>
+        <ImageThumbnail />
+        </div>
+
+    );
+}
+
+export default HeaderContainer;
